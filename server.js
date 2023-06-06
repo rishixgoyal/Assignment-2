728bf066f1b79e2b938a95b8bf92b812fbce97f5
RISHI GOYAL <rgoyal13@myseneca.ca>
2023-06-05 23:07:21 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Rishi Goyal Student ID: 163430218 Date: 4 June,2023
+*
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: https://github.com/rishixgoyal/Assignment-2.git
+*
+********************************************************************************/ 
+
+const express = require("express");
+const path = require("path");
+const { initialize, getAllPosts, getPublishedPosts, getCategories } = require("./blog-service.js");
+
+const app = express();
+
+app.use(express.static('public')); 
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req,res) => {
+  res.redirect("/about");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+})
+
+
+app.get("/blog", (req, res) => {
+  getPublishedPosts()
+  .then((data) => {
+    res.send(data)
+  })
+
+  .catch((err) => {
+    res.send(err);
+  })
+});
+
+
+app.get("/posts", (req, res) => {
+  getAllPosts()
+    .then((data) => {
+      res.send(data)
+    })
+
+    .catch((err) => {
+      res.send(err);
+    })
+});
+
+app.get("/categories", (req, res) => {
+  getCategories()
+  .then((data) => {
+    res.send(data)
+  })
+ 
+  .catch((err) => {
+    res.send(err);
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "notFoundPage.html"));
+})
+
+initialize().then(() => {
+  app.listen(HTTP_PORT, () => {
+    console.log("Express http server listening on: " + HTTP_PORT);
+  });
+})
